30645284681fe1eec061b80e81e7178575a3ee64
Charles Loureiro de Deus <charliedeus@gmail.com>
2021-03-07 16:20:13 -0300

    Styles from first component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,22 @@
+import * as S from './styles';
+
 const Main = () => {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        Typescript, ReactJS, NextJS e Styled Components
+      </S.Description>
+
+      <S.Illustration
+        src="/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código."
+      />
+    </S.Wrapper>
   );
 };
 
